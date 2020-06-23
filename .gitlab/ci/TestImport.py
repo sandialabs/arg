7b74b5e8e242e053001af4f9fc22e97dd952fb13
Yale Lee <yale.lee@ng-analytics.com>
2020-06-23 11:12:44 +0200

    #1: first CI instantiation

---
+++ .gitlab/ci/TestImport.py
@@ -0,0 +1,10 @@
+import numpy
+print(numpy.__path__)
+import yaml
+print(yaml.__path__)
+import pylatex
+print(pylatex.__path__)
+import docx
+print(docx.__path__)
+import matplotlib
+print(matplotlib.__path__)
